98eeb573bd7a4c62364df01353617a15c6097da5
Quan Nguyen <qmnguyen@iu.edu>
2023-03-02 15:19:55 -0500

    Config neovim from scratch

---
+++ nvim/after/plugin/lsp.lua
@@ -0,0 +1,6 @@
+local lsp = require('lsp-zero').preset('recommended')
+
+-- (Optional) Configure lua language server for neovim
+lsp.nvim_workspace()
+lsp.setup()
+
---
+++ nvim/lua/config/set.lua
@@ -0,0 +1,22 @@
+-- Enable line number and relative line numbers.
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+-- Indentation.
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+-- Searching.
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
+-- Scrolling.
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+-- Update time.
+vim.opt.updatetime = 50
