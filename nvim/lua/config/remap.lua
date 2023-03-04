d89725b5970ca7dc1b412ca3f30eadcc5c54ce06
beekill <nguyenmbquan95@gmail.com>
2023-03-03 20:45:23 -0500

    Add some useful keymaps for windows and text selection

--- nvim/lua/config/remap.lua
+++ nvim/lua/config/remap.lua
@@ -1,2 +1,30 @@
+local opts = { noremap = true }
+
+-- Leader.
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+
+-- Shoten kepmap function.
+local keymap = vim.keymap.set
+
+-- Open default file explorer.
+keymap("n", "<leader>e", vim.cmd.Ex, opts)
+
+-- WINDOWS.
+-- Move between windows.
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+
+-- Resize windows.
+keymap("n", "<C-Up>", ":resize -2<CR>", opts)
+keymap("n", "<C-Down>", ":resize +2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+-- TEXT SELECTION.
+-- Move text up and down.
+keymap("v", "<A-j>", ":move '>+1<CR>gv=gv", opts)
+keymap("v", "<A-k>", ":move '<-2<CR>gv=gv", opts)
+keymap("x", "<A-j>", ":move '>+1<CR>gv=gv", opts)
+keymap("x", "<A-k>", ":move '<-2<CR>gv=gv", opts)
