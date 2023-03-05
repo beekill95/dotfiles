080498313e98c39d75b3db6339e01c1b5e0e6a33
beekill <nguyenmbquan95@gmail.com>
2023-03-05 13:17:27 -0500

    Automatically enable line number in neotree's buffer

--- nvim/after/plugin/neo-tree.lua
+++ nvim/after/plugin/neo-tree.lua
@@ -10,11 +10,40 @@ vim.keymap.set("n", "<C-e>", ":NeoTreeFocusToggle<cr>", {})
 -- Config.
 neo_tree.setup({
     window = {
+        position = "left",
+        width = 40,
         mappings = {
             ["o"] = {
                 command = "open",
                 nowait = true
             },
         }
-    }
+    },
+    event_handlers = {
+        {
+            event = "neo_tree_window_after_open",
+            handler = function(args)
+                -- Setup autocmd to automatically enable/disable line number.
+                local winid = args.winid
+                local bufid = vim.api.nvim_win_get_buf(winid)
+                vim.api.nvim_create_augroup("NeotreeLineNumber", { clear = true })
+                vim.api.nvim_create_autocmd("BufEnter", {
+                    group = "NeotreeLineNumber",
+                    buffer = bufid,
+                    desc = "Enable line number for Neotree buffer.",
+                    command = "setlocal number relativenumber",
+                })
+                vim.api.nvim_create_autocmd("BufLeave", {
+                    group = "NeotreeLineNumber",
+                    buffer = bufid,
+                    desc = "Disable line number for Neotree buffer.",
+                    command = "setlocal nonumber norelativenumber",
+                })
+
+                -- Enable line number in the new window.
+                vim.api.nvim_win_set_option(winid, "number", true)
+                vim.api.nvim_win_set_option(winid, "relativenumber", true)
+            end
+        },
+    },
 })
