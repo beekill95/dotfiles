ccf22703c7968c96e4e6210f9a71cdf7c08b420d
Quan Nguyen <qmnguyen@iu.edu>
2023-03-11 14:18:17 -0500

    Rewrite automatically show/hide line number in neo-tree buffer

--- nvim/after/plugin/neo-tree.lua
+++ nvim/after/plugin/neo-tree.lua
@@ -19,31 +19,24 @@ neo_tree.setup({
             },
         }
     },
-    event_handlers = {
-        {
-            event = "neo_tree_window_after_open",
-            handler = function(args)
-                -- Setup autocmd to automatically enable/disable line number.
-                local winid = args.winid
-                local bufid = vim.api.nvim_win_get_buf(winid)
-                vim.api.nvim_create_augroup("NeotreeLineNumber", { clear = true })
-                vim.api.nvim_create_autocmd("BufEnter", {
-                    group = "NeotreeLineNumber",
-                    buffer = bufid,
-                    desc = "Enable line number for Neotree buffer.",
-                    command = "setlocal number relativenumber",
-                })
-                vim.api.nvim_create_autocmd("BufLeave", {
-                    group = "NeotreeLineNumber",
-                    buffer = bufid,
-                    desc = "Disable line number for Neotree buffer.",
-                    command = "setlocal nonumber norelativenumber",
-                })
-
-                -- Enable line number in the new window.
-                vim.api.nvim_win_set_option(winid, "number", true)
-                vim.api.nvim_win_set_option(winid, "relativenumber", true)
-            end
-        },
-    },
 })
+
+-- Automatically show/hide line number in neotree buffer when enter/leave.
+function setupLineNumberInNeotreeBuffer()
+    vim.api.nvim_create_autocmd({"BufEnter", "BufLeave"}, {
+        group = vim.api.nvim_create_augroup("NeotreeLineNumber", { clear = true }),
+        desc = "Enable/Disable line number for Neotree buffer.",
+        -- command = "setlocal number relativenumber",
+        callback = function(args)
+            if vim.bo[args.buf].filetype == 'neo-tree' then
+                if args.event == 'BufEnter' then
+                    vim.cmd "setlocal number relativenumber"
+                else
+                    vim.cmd "setlocal nonumber norelativenumber"
+                end
+                    
+            end
+        end,
+    })
+end
+setupLineNumberInNeotreeBuffer()
