e6e1a5811fc2f1f6b979eaf901464126a5a1770b
beekill <nguyenmbquan95@gmail.com>
2024-05-20 22:06:25 +0700

    Disable neorg config

--- nvim/after/plugin/neorg.lua
+++ nvim/after/plugin/neorg.lua
@@ -1,21 +1,21 @@
-local status_ok, neorg = pcall(require, "neorg")
-if not status_ok then
-    vim.notify("Neorg plugin not found! Quit configuring!")
-    return
-end
+-- local status_ok, neorg = pcall(require, "neorg")
+-- if not status_ok then
+--     vim.notify("Neorg plugin not found! Quit configuring!")
+--     return
+-- end
 
-neorg.setup {
-    load = {
-        ["core.defaults"] = {}, -- Loads default behaviour
-        ["core.concealer"] = {}, -- Adds pretty icons to your documents
-        -- Manages Neorg workspaces
-        ["core.dirman"] = {
-            config = {
-                workspaces = {
-                    zettelkasten = "~/zettelkasten",
-                },
-                default_workspace = "zettelkasten",
-            },
-        },
-    },
-}
+-- neorg.setup {
+--     load = {
+--         ["core.defaults"] = {}, -- Loads default behaviour
+--         ["core.concealer"] = {}, -- Adds pretty icons to your documents
+--         -- Manages Neorg workspaces
+--         ["core.dirman"] = {
+--             config = {
+--                 workspaces = {
+--                     zettelkasten = "~/zettelkasten",
+--                 },
+--                 default_workspace = "zettelkasten",
+--             },
+--         },
+--     },
+-- }
