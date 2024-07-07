5f5e31a591550b07372cd7a6c6bf9f9bba355cd1
beekill <nguyenmbquan95@gmail.com>
2024-07-07 16:49:26 +0700

    Add group to autoread functionality

--- nvim/lua/config/autoread.lua
+++ nvim/lua/config/autoread.lua
@@ -3,6 +3,7 @@
 -- and then go back to vim for further editting.
 vim.api.nvim_command(":set autoread")
 vim.api.nvim_create_autocmd({"FocusGained", "CursorHold"}, {
+    group = vim.api.nvim_create_augroup("reload_python_notebook", { clear = true }),
     -- Usually, this is just .py files.
     pattern = { "*.py" },
     command = ":checktime",
