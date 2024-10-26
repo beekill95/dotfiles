533d0c47ae34e6cb797d72aef83bed9a3692f992
beekill <nguyenmbquan95@gmail.com>
2024-10-26 11:40:04 +0700

    Storing obsidian notes at subdir "notes" and change the default naming scheme

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -24,5 +24,25 @@ return {
                 path = "~/vaults/zettelkasten",
             },
         },
+        notes_subdir = "notes",
+        -- Optional, customize how note IDs are generated given an optional title.
+        ---@param title string|?
+        ---@return string
+        note_id_func = function(title)
+            -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+            -- The timestamp is formatted as YYYYMMddHHMM for easier sort.
+            local suffix = ""
+            if title ~= nil then
+                -- If title is given, transform it into valid file name.
+                suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+            else
+                -- If title is nil, just add 4 random uppercase letters to the suffix.
+                for _ = 1, 4 do
+                    suffix = suffix .. string.char(math.random(65, 90))
+                end
+            end
+            local prefix = tostring(os.date("%Y%m%d%H%M", os.time()))
+            return prefix .. "-" .. suffix
+        end,
     },
 }
