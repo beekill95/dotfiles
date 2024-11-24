0b3f45f288d25667cc8c9fd5eecd7786fcb5ee75
beekill <nguyenmbquan95@gmail.com>
2024-11-24 07:47:52 +0700

    Revert changing to powershell due to lazygit not working

--- nvim/lua/config/win32.lua
+++ nvim/lua/config/win32.lua
@@ -1,8 +1,9 @@
 -- Windows' specific configuration.
 
+-- FIXME: right now doesn't work with lazygit!
 -- Set the default shell.
-vim.cmd [[let &shell = 'powershell.exe']]
+-- vim.cmd [[let &shell = 'powershell.exe']]
 -- This is to start the developer powershell for VS 2022
 -- It is copied from Windows' Terminal setting.
 -- Note that: replace the triple double-quotes (""") with \"
-vim.cmd [[let &shellcmdflag = '-NoExit -Command "&{Import-Module \"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\Microsoft.VisualStudio.DevShell.dll\"; Enter-VsDevShell 6a27c4d6 -SkipAutomaticLocation -DevCmdArguments \"-arch=x64 -host_arch=x64\"}"']]
+-- vim.cmd [[let &shellcmdflag = '-NoExit -Command "&{Import-Module \"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\Microsoft.VisualStudio.DevShell.dll\"; Enter-VsDevShell 6a27c4d6 -SkipAutomaticLocation -DevCmdArguments \"-arch=x64 -host_arch=x64\"}"']]
