863d19d50641f41f902b3ed9e3f3eb00241b35f2
beekill <nguyenmbquan95@gmail.com>
2025-03-06 14:25:15 +0700

    Update lsp config to accept enter and tab to accept/select suggestions

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -104,10 +104,46 @@ return {
             },
             snippet = {
                 expand = function(args)
-                    vim.snippet.expand(args.body)
+                    require('luasnip').lsp_expand(args.body)
                 end,
             },
-            mapping = cmp.mapping.preset.insert({}),
+            window = {
+                completion = cmp.config.window.bordered(),
+                documentation = cmp.config.window.bordered(),
+            },
+            mapping = cmp.mapping.preset.insert({
+                -- Confirm selection.
+                ['<CR>'] = cmp.mapping.confirm({select = true}),
+                -- Move up & down between sugesstions using Super Tab.
+                -- Super tab and super shift-tab.
+                ['<Tab>'] = cmp.mapping(function(fallback)
+                    local luasnip = require('luasnip')
+                    local col = vim.fn.col('.') - 1
+
+                    if cmp.visible() then
+                        cmp.select_next_item({behavior = 'select'})
+                    elseif luasnip.expand_or_locally_jumpable() then
+                        luasnip.expand_or_jump()
+                    elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
+                        fallback()
+                    else
+                        cmp.complete()
+                    end
+                end, {'i', 's'}),
+
+                -- Super shift tab
+                ['<S-Tab>'] = cmp.mapping(function(fallback)
+                    local luasnip = require('luasnip')
+
+                    if cmp.visible() then
+                        cmp.select_prev_item({behavior = 'select'})
+                    elseif luasnip.locally_jumpable(-1) then
+                        luasnip.jump(-1)
+                    else
+                        fallback()
+                    end
+                end, {'i', 's'}),
+            }),
         })
 
         -- Finishing touch.
